da25f2dcc169c3a4dbb14174dacac751e616e08d
Luciano Minni <luciano.minni@agictech.com>
2024-06-24 10:04:22 +0200

    feat: add npm resend

--- src/server/api/routers/test.ts
+++ src/server/api/routers/test.ts
@@ -16,7 +16,7 @@ export const testRouter = createTRPCRouter({
 
             try {
                 Logger.info("sending mail");
-                ReSendMailAsync('ErFantacalcio: test', 'lucianominni@gmail.com', 'Notifica automatica da erFantacalcio.com');
+                await ReSendMailAsync('ErFantacalcio: test', 'lucianominni@gmail.com', 'Notifica automatica da erFantacalcio.com');
                 Logger.info("mail sent");
             } catch (error) {
                 Logger.error('Si è verificato un errore', error);
