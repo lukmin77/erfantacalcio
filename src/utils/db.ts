25b648319b00974051827ef521d60a29c46e11fb
Luciano Minni <luciano.minni@agictech.com>
2024-06-24 09:25:41 +0200

    feat: add log queries

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,7 +1,26 @@
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  return new PrismaClient({
+    log: [
+        {
+            emit: 'event',
+            level: 'query',
+        },
+        {
+            emit: 'stdout',
+            level: 'error',
+        },
+        {
+            emit: 'stdout',
+            level: 'info',
+        },
+        {
+            emit: 'stdout',
+            level: 'warn',
+        },
+    ],
+})
 }
 
 declare const globalThis: {
@@ -9,6 +28,11 @@ declare const globalThis: {
 } & typeof global;
 
 const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+prisma.$on('query', (e) => {
+  console.log('Query: ' + e.query)
+  console.log('Params: ' + e.params)
+  console.log('Duration: ' + e.duration + 'ms')
+});
 
 export default prisma
 
