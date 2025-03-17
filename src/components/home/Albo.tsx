9523165908787ef6f10ad023370c4aa347c1add3
Luciano Minni <luciano.minni@agictech.com>
2025-03-17 11:17:47 +0100

    refactor: update Albo component to use DataGrid and improve loading state handling

--- src/components/home/Albo.tsx
+++ src/components/home/Albo.tsx
@@ -1,37 +1,105 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import { api } from "~/utils/api";
-import DataTable, { type Column } from "~/components/tables/datatable";
-import { Box, CircularProgress } from "@mui/material";
-
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import { autosizeOptions } from "~/utils/datatable";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 export default function Albo() {
-    const alboList = api.albo.list.useQuery(undefined, { refetchOnWindowFocus: false, refetchOnReconnect: false });
-    
-    const columns: Column[] = [
-        { key: "stagione", type: "string", align:'left'},
-        { key: "campionato", type: "string", align:'left'},
-        { key: "champions", type: "string", align:'left'},
-        { key: "secondo", type: "string", align:'left'},
-        { key: "terzo", type: "string", align:'left'},
-    ];
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.down("md"));
+  const alboList = api.albo.list.useQuery(undefined, {
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
 
-    return (
-        <>
-            {alboList.isLoading ? (
-                <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                    <CircularProgress color="warning" />
-                </Box>
-            ) : (
-                <DataTable
-                    title={`Albo d'oro`}
-                    pagination={false}
-                    data={alboList.data}
-                    errorMessage={''}
-                    columns={columns}
-                />
-            )}
+  const columns: GridColDef[] = [
+    { field: "id", hideable: true },
+    {
+      field: "stagione",
+      type: "string",
+      align: "left",
+      renderHeader: () => <strong>Stagione</strong>,
+      flex: isXs ? 0 : 1,
+    },
+    {
+      field: "campionato",
+      type: "string",
+      align: "left",
+      renderHeader: () => <strong>Campionato</strong>,
+      flex: isXs ? 0 : 1,
+    },
+    {
+      field: "champions",
+      type: "string",
+      align: "left",
+      renderHeader: () => <strong>Champions</strong>,
+      flex: isXs ? 0 : 1,
+    },
+    {
+      field: "secondo",
+      type: "string",
+      align: "left",
+      renderHeader: () => <strong>Secondo</strong>,
+      flex: isXs ? 0 : 1,
+    },
+    {
+      field: "terzo",
+      type: "string",
+      align: "left",
+      renderHeader: () => <strong>Terzo</strong>,
+      flex: isXs ? 0 : 1,
+    },
+  ];
 
-            
-        </>);
+  const pageSize = 20;
+
+  const skeletonRows = Array.from({ length: pageSize }, (_, index) => ({
+    id: `skeleton-${index}`,
+  }));
+
+  return (
+    <Box sx={{ width: "100%", overflowX: "auto", contain: "inline-size" }}>
+      <DataGrid
+        columnHeaderHeight={45}
+        rowHeight={40}
+        loading={alboList.isLoading}
+        initialState={{
+          columns: {
+            columnVisibilityModel: {
+              id: false,
+            },
+          },
+          pagination: undefined,
+          filter: undefined,
+          density: "compact",
+        }}
+        slotProps={{
+          loadingOverlay: {
+            variant: "skeleton",
+          },
+        }}
+        checkboxSelection={false}
+        disableColumnFilter={true}
+        disableColumnMenu={true}
+        disableColumnSelector={true}
+        disableColumnSorting={true}
+        disableColumnResize={true}
+        hideFooter={true}
+        hideFooterPagination={true}
+        hideFooterSelectedRowCount={true}
+        columns={columns}
+        rows={alboList.isLoading ? skeletonRows : alboList.data}
+        disableRowSelectionOnClick={true}
+        autosizeOptions={autosizeOptions}
+        sx={{
+          backgroundColor: "#fff",
+          "& .MuiDataGrid-columnHeader": {
+            color: theme.palette.primary.main,
+            backgroundColor: theme.palette.secondary.light,
+          },
+        }}
+      />
+    </Box>
+  );
 }
