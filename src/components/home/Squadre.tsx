a41f6831ed5f01251efd42914f1de86fb113f882
Luciano Minni <luciano.minni@agictech.com>
2025-03-19 17:42:18 +0100

    refactor: optimize loading state handling in Squadre component

--- src/components/home/Squadre.tsx
+++ src/components/home/Squadre.tsx
@@ -6,7 +6,6 @@ import {
   CardActionArea,
   CardContent,
   CardMedia,
-  Skeleton,
   Typography,
 } from "@mui/material";
 
@@ -26,35 +25,28 @@ export default function Squadre() {
         flexWrap: "wrap",
       }}
     >
-      {!squadreList.isLoading
-        ? squadreList.data?.map((squadra, index) => (
-            <Card
-              key={index}
-              sx={{ minWidth: 130, maxWidth: 130, marginBottom: "3px" }}
-            >
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="90"
-                  image={squadra.foto ?? ""}
-                  alt={squadra.squadra}
-                  onClick={() =>
-                    (window.location.href = `/squadra/${squadra.id}/${squadra.squadra}`)
-                  }
-                />
-                <CardContent sx={{ paddingBottom: "2px", paddingLeft: "6px" }}>
-                  <Typography variant="h5">{squadra.squadra}</Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))
-        : Array.from({ length: 8 }, (_, index) => (
-            <Box key={index} sx={{ minWidth: 130, maxWidth: 130 }}>
-              <Skeleton width="100%" height={130} animation="pulse">
-                <Typography>.</Typography>
-              </Skeleton>
-            </Box>
-          ))}
+      {!squadreList.isLoading &&
+        squadreList.data?.map((squadra, index) => (
+          <Card
+            key={index}
+            sx={{ minWidth: 130, maxWidth: 130, marginBottom: "3px" }}
+          >
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="90"
+                image={squadra.foto ?? ""}
+                alt={squadra.squadra}
+                onClick={() =>
+                  (window.location.href = `/squadra/${squadra.id}/${squadra.squadra}`)
+                }
+              />
+              <CardContent sx={{ paddingBottom: "2px", paddingLeft: "6px" }}>
+                <Typography variant="h5">{squadra.squadra}</Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
     </Box>
   );
 }
