65d1ff34545de7aa4a5b2d8256fc385fd9876a9c
Luciano Minni <luciano.minni@agictech.com>
2025-03-19 16:24:20 +0100

    refactor: add loading skeleton and error handling in Calendario component

--- src/components/home/Calendario.tsx
+++ src/components/home/Calendario.tsx
@@ -1,45 +1,82 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { api } from "~/utils/api";
-import { Alert, Stack } from "@mui/material";
-import { type GiornataType } from '~/types/common';
-import CardPartite from '../cardPartite/CardPartite';
-import CheckIcon from '@mui/icons-material/CheckCircle';
+import { Alert, Box, Skeleton, Stack, Typography } from "@mui/material";
+import { type GiornataType } from "~/types/common";
+import CardPartite from "../cardPartite/CardPartite";
+import CheckIcon from "@mui/icons-material/CheckCircle";
 
 interface CalendarioProps {
-    prefixTitle: string;
-    tipo: 'risultati' | 'prossima';
+  prefixTitle: string;
+  tipo: "risultati" | "prossima";
 }
 
 export default function Calendario({ prefixTitle, tipo }: CalendarioProps) {
-    const calendarioList = tipo === 'prossima' 
-        ? api.calendario.getProssimeGiornate.useQuery(undefined, { refetchOnWindowFocus: false, refetchOnReconnect: false })
-        : api.calendario.getUltimiRisultati.useQuery(undefined, { refetchOnWindowFocus: false, refetchOnReconnect: false });
-    const [errorMessage, setErrorMessage] = useState('');
-    const [giornata, setGiornata] = useState<GiornataType[]>();
+  const calendarioList =
+    tipo === "prossima"
+      ? api.calendario.getProssimeGiornate.useQuery(undefined, {
+          refetchOnWindowFocus: false,
+          refetchOnReconnect: false,
+        })
+      : api.calendario.getUltimiRisultati.useQuery(undefined, {
+          refetchOnWindowFocus: false,
+          refetchOnReconnect: false,
+        });
+  const [errorMessage, setErrorMessage] = useState("");
+  const [giornata, setGiornata] = useState<GiornataType[]>();
 
-    useEffect(() => {
-        if (!calendarioList.isFetching && calendarioList.isSuccess && calendarioList.data) {
-            setGiornata(calendarioList.data);
-        }
-    }, [calendarioList.data, calendarioList.isSuccess, calendarioList.isFetching]);
+  useEffect(() => {
+    if (
+      !calendarioList.isFetching &&
+      calendarioList.isSuccess &&
+      calendarioList.data
+    ) {
+      setGiornata(calendarioList.data);
+    }
+  }, [
+    calendarioList.data,
+    calendarioList.isSuccess,
+    calendarioList.isFetching,
+  ]);
 
+  useEffect(() => {
+    if (calendarioList.isError) {
+      setErrorMessage("Si è verificato un errore in fase di caricamento");
+    }
+  }, [calendarioList.isError]);
 
-    useEffect(() => {
-        if (calendarioList.isError) {
-            setErrorMessage('Si è verificato un errore in fase di caricamento');
-        }
-    }, [calendarioList.isError]);
-
-    return (
-        <>
-            {!calendarioList.isLoading && giornata && (
-                <CardPartite giornata={giornata} prefixTitle={prefixTitle} maxWidth={600} withAvatar={true}></CardPartite>
-            )}
-            {errorMessage && (
-                <Stack sx={{ width: '100%' }} spacing={0}>
-                    <Alert icon={<CheckIcon fontSize="inherit" />} severity="error">{errorMessage}</Alert>
-                </Stack>
-            )}
-        </>
-    );
+  return (
+    <>
+      {!calendarioList.isLoading && giornata && (
+        <CardPartite
+          giornata={giornata}
+          prefixTitle={prefixTitle}
+          maxWidth={600}
+          withAvatar={true}
+        ></CardPartite>
+      )}
+      {calendarioList.isLoading && (
+        <Box>
+          <Skeleton width="100%" height={70} animation="pulse">
+            <Typography>.</Typography>
+          </Skeleton>
+          <Skeleton width="100%" height={70} animation="pulse">
+            <Typography>.</Typography>
+          </Skeleton>
+          <Skeleton width="100%" height={70} animation="pulse">
+            <Typography>.</Typography>
+          </Skeleton>
+          <Skeleton width="100%" height={70} animation="pulse">
+            <Typography>.</Typography>
+          </Skeleton>
+        </Box>
+      )}
+      {errorMessage && (
+        <Stack sx={{ width: "100%" }} spacing={0}>
+          <Alert icon={<CheckIcon fontSize="inherit" />} severity="error">
+            {errorMessage}
+          </Alert>
+        </Stack>
+      )}
+    </>
+  );
 }
