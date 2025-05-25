3638875a131ecb044ee1f8df9cfb18505749ae91
luciano minni <lucianominni@gmail.com>
2025-05-25 19:26:17 +0200

    feat: refactor SquadraPage to include StatisticaSquadra component and update Rosa layout

--- src/components/squadra/Rosa.tsx
+++ src/components/squadra/Rosa.tsx
@@ -94,10 +94,10 @@ function Rosa({ idSquadra, squadra }: RosaProps) {
               flexWrap: 'wrap',
             }}
           >
-            <Card sx={{ marginBottom: '1px' }}>
+            <Card sx={{ marginBottom: '1px', maxWidth: '180px' }}>
               <CardMedia
                 component="img"
-                sx={{ cursor: 'pointer', width: '120px' }}
+                sx={{ cursor: 'pointer', width: '60' }}
                 image={giocatore.urlCampioncino}
                 alt={giocatore.nome}
                 onClick={() => handleGiocatoreSelected(giocatore.idGiocatore)}
@@ -162,7 +162,7 @@ function Rosa({ idSquadra, squadra }: RosaProps) {
     <>
       <Grid container spacing={0}>
         <Grid item xs={12}>
-          <Typography variant="h3">Rosa {squadra}</Typography>
+          <Typography variant="h4">Rosa</Typography>
         </Grid>
         {rosaList.isLoading ? (
           <Box
