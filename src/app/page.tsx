7d5ed3ef34f5cb0056e5d16f13ed7eb37d0eb925
luciano minni <lucianominni@gmail.com>
2025-07-04 10:27:19 +0200

    fix: update rendering condition to display components based on Serie A start date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -256,7 +256,7 @@ export default function HomePage() {
                 <SquadreXs />
               </Grid>
             )}
-            {!isXs && (
+            {(!isXs || new Date() >= Configurazione.dataGiornata1SerieA) && (
               <>
                 <Zoom in={true}>
                   <Grid
