08d7429ce86ae80929b6813ee1f2bad96b7c9fa9
luciano minni <lucianominni@gmail.com>
2025-11-15 19:08:48 +0100

    refactor: update data source configuration and add custom naming strategy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,7 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
-import { Utenti } from './server/db/entities/Utenti.js'
-import { Calendario } from './server/db/entities/Calendario.js'
-import { Tornei } from './server/db/entities/Tornei.js'
-import { Partite } from './server/db/entities/Partite.js'
-import { Formazioni } from './server/db/entities/Formazioni.js'
-import { Giocatori } from './server/db/entities/Giocatori.js'
-import { Trasferimenti } from './server/db/entities/Trasferimenti.js'
-import { SquadreSerieA } from './server/db/entities/SquadreSerieA.js'
-import { Classifiche } from './server/db/entities/Classifiche.js'
-import { Voti } from './server/db/entities/Voti.js'
 import 'dotenv/config'
+import { NamingStrategy } from './server/db/utils/namingStrategy.js'
 
 // Incremental migration: do NOT enable synchronize in production.
 export const AppDataSource = new DataSource({
@@ -21,18 +12,8 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   migrations: ['src/server/db/migrations/*.ts'],
-  entities: [
-    Utenti,
-    Calendario,
-    Tornei,
-    Partite,
-    Formazioni,
-    Giocatori,
-    Trasferimenti,
-    SquadreSerieA,
-    Classifiche,
-    Voti,
-  ],
+  namingStrategy: new NamingStrategy(),
+  entities: ['src/server/db/entities/*.ts'],
   synchronize: false,
   logging: false,
 })
---
+++ src/server/db/utils/namingStrategy.ts
@@ -0,0 +1,53 @@
+import { DefaultNamingStrategy, Table } from 'typeorm'
+
+export class NamingStrategy extends DefaultNamingStrategy {
+  foreignKeyName(
+    tableOrName: string | Table,
+    columnNames: string[],
+    referencedTablePath?: string | Table | undefined,
+    referencedColumnNames?: string[] | undefined,
+  ): string {
+    // Produce FK_<Table>_<ReferencedTable>
+    const tableName = this.getTableName(tableOrName).replace('.', '_')
+    const refName = referencedTablePath
+      ? this.getTableName(referencedTablePath as any).replace('.', '_')
+      : (referencedColumnNames ? referencedColumnNames.join('_') : '')
+    return `FK_${tableName}_${refName}`
+  }
+//   joinColumnName(relationName: string, referencedColumnName: string): string {
+//     //console.log('joincolumnname: ', underscore(`${relationName}_${referencedColumnName}`));
+//     return underscore(`${relationName}_${referencedColumnName}`)
+//   }
+//   joinTableColumnName(tableName: string, propertyName: string, columnName?: string | undefined): string {
+//     //console.log('joinTableColumnName: ', underscore(`${tableName}_${columnName ? columnName : propertyName}`));
+//     return underscore(`${singularize(tableName)}_${columnName ?? propertyName}`)
+//   }
+//   joinTableInverseColumnName(tableName: string, propertyName: string, columnName?: string | undefined): string {
+//     //console.log('joinTableInverseColumnName');
+//     return this.joinTableColumnName(tableName, propertyName, columnName)
+//   }
+
+  private constraintName(prefix: string, columnNames: string[], tableOrName: string | Table) {
+    const clonedColumnNames = [...columnNames]
+    clonedColumnNames.sort()
+    const tableName = this.getTableName(tableOrName)
+    const replacedTableName = tableName.replace('.', '_')
+    const key = `${replacedTableName}_${clonedColumnNames.join('_')}`
+    return `${prefix}_${key}`.substring(0, 128).toUpperCase()
+  }
+
+//   uniqueConstraintName(tableOrName: Table | string, columnNames: string[]) {
+//     const clonedColumnNames = [...columnNames]
+//     clonedColumnNames.sort()
+//     const tableName = this.getTableName(tableOrName)
+//     const replacedTableName = tableName.replace('.', '_')
+//     const key = `${replacedTableName}_${clonedColumnNames.join('_')}`
+//     return `UQ_${key}`.substring(0, 128)
+//   }
+
+//   defaultConstraintName(tableOrName: Table | string, columnName: string) {
+//     const tableName = this.getTableName(tableOrName)
+//     const replacedTableName = tableName.replace('.', '_')
+//     return `DF_${replacedTableName}__${columnName}`.substring(0, 128)
+//   }
+}
