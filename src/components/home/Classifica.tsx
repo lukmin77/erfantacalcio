16987d05e6eb42cea32ba89d28e518636fca928a
luciano minni <lucianominni@gmail.com>
2025-11-25 13:21:58 +0100

    refactor: Remove unused interface ClassificaProps for cleaner code

--- src/components/home/Classifica.tsx
+++ src/components/home/Classifica.tsx
@@ -8,17 +8,12 @@ import { autosizeOptions } from '~/utils/datatable'
 import { z } from 'zod'
 import { classificaSchema } from '~/schemas/classifica'
 
-interface ClassificaProps {
-  nomeTorneo: string
-  idTorneo: number | undefined
-  gruppo: string | null
-}
 
 export default function Classifica({
   nomeTorneo = '',
   idTorneo = undefined,
   gruppo = null,
-}: ClassificaProps) {
+}) {
   const theme = useTheme()
   const isXs = useMediaQuery(theme.breakpoints.down('md'))
 
