f9857b1c2e361a1eab7527fdf566799672f5501b
luciano minni <lucianominni@gmail.com>
2025-12-09 16:08:35 +0100

    fix: Add check for idGiocatore before creating or updating Voti records to prevent invalid entries

--- src/server/api/voti/procedures/processVoti.ts
+++ src/server/api/voti/procedures/processVoti.ts
@@ -74,48 +74,50 @@ export const processVotiProcedure = adminProcedure
             }
 
             // Upsert con update e create uguali
-            const votoSave = trx.create(Voti, {
-              voto: votoGiocatore.Voto ?? 0,
-              ammonizione:
-                votoGiocatore.Ammonizione === 1
-                  ? Configurazione.bonusAmmonizione
-                  : 0,
-              espulsione:
-                votoGiocatore.Espulsione === 1
-                  ? Configurazione.bonusEspulsione
-                  : 0,
-              gol:
-                votoGiocatore.Ruolo === 'P'
-                  ? votoGiocatore.GolSubiti * Configurazione.bonusGolSubito
-                  : votoGiocatore.GolSegnati * Configurazione.bonusGol,
-              assist: votoGiocatore.Assist * Configurazione.bonusAssist,
-              autogol: votoGiocatore.Autogol * Configurazione.bonusAutogol,
-              altriBonus:
-                (votoGiocatore.RigoriParati ?? 0) *
-                  Configurazione.bonusRigoreParato +
-                (votoGiocatore.RigoriErrati ?? 0) *
-                  Configurazione.bonusRigoreSbagliato,
-            })
-
-            const criteria = {
-              idGiocatore: idGiocatore,
-              idCalendario: opts.input.idCalendario,
-            }
-            const isExists = await trx.exists(Voti, {
-              where: criteria,
-            })
-
-            // remove id fields from the data to avoid specifying them twice when spreading
-            const votoData = _.omit(votoSave, ['idCalendario', 'idGiocatore'])
-
-            if (isExists) {
-              await trx.update(Voti, criteria, votoData)
-            } else {
-              await trx.insert(Voti, {
-                idCalendario: opts.input.idCalendario,
-                idGiocatore: idGiocatore,
-                ...votoData,
+            if (idGiocatore !== 0) {
+              const votoSave = trx.create(Voti, {
+                voto: votoGiocatore.Voto ?? 0,
+                ammonizione:
+                  votoGiocatore.Ammonizione === 1
+                    ? Configurazione.bonusAmmonizione
+                    : 0,
+                espulsione:
+                  votoGiocatore.Espulsione === 1
+                    ? Configurazione.bonusEspulsione
+                    : 0,
+                gol:
+                  votoGiocatore.Ruolo === 'P'
+                    ? votoGiocatore.GolSubiti * Configurazione.bonusGolSubito
+                    : votoGiocatore.GolSegnati * Configurazione.bonusGol,
+                assist: votoGiocatore.Assist * Configurazione.bonusAssist,
+                autogol: votoGiocatore.Autogol * Configurazione.bonusAutogol,
+                altriBonus:
+                  (votoGiocatore.RigoriParati ?? 0) *
+                    Configurazione.bonusRigoreParato +
+                  (votoGiocatore.RigoriErrati ?? 0) *
+                    Configurazione.bonusRigoreSbagliato,
               })
+
+              const criteria = {
+                idGiocatore: idGiocatore,
+                idCalendario: opts.input.idCalendario,
+              }
+              const isExists = await trx.exists(Voti, {
+                where: criteria,
+              })
+
+              // remove id fields from the data to avoid specifying them twice when spreading
+              const votoData = _.omit(votoSave, ['idCalendario', 'idGiocatore'])
+
+              if (isExists) {
+                await trx.update(Voti, criteria, votoData)
+              } else {
+                await trx.insert(Voti, {
+                  idCalendario: opts.input.idCalendario,
+                  idGiocatore: idGiocatore,
+                  ...votoData,
+                })
+              }
             }
 
             console.log(`Processed voto for player: ${votoGiocatore.Nome}`)
@@ -314,7 +316,7 @@ async function checkFormazioni(idCalendario: number) {
       Partite: {
         idPartita: true,
         Formazioni: {
-          idFormazione: true
+          idFormazione: true,
         },
       },
     },
